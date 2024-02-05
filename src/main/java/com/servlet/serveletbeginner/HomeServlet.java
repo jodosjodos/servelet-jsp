1f6ddeada13368bd0693417faa1281d27b8964c2
jodosjodos <jeandedieu2030@gmail.com>
2024-02-05 11:57:38 +0200

    add servlet config

--- src/main/java/com/servlet/serveletbeginner/HomeServlet.java
+++ src/main/java/com/servlet/serveletbeginner/HomeServlet.java
@@ -1,5 +1,6 @@
 package com.servlet.serveletbeginner;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,9 +15,14 @@ public class HomeServlet extends HttpServlet {
             PrintWriter out = res.getWriter();
             out.println("Hi ");
 
+//            using servlet context
             ServletContext ctx = req.getServletContext();
-        String str=    ctx.getInitParameter("phoneNumber");
-        out.println(str);
+            String str = ctx.getInitParameter("phoneNumber");
+
+//        using servlet config
+            ServletConfig config = getServletConfig();
+            String strConfig = config.getInitParameter("name");
+            out.println(str + "and config name is : " + strConfig);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
